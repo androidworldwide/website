c77423efd0f923baebc47e4179840d7c542724e0
Faiz Malkani <malkanifaiz@gmail.com>
2020-10-29 13:35:34 +0530

    Fixed the error in the last commit of not setting the attribute

--- public/js/data.js
+++ public/js/data.js
@@ -10,17 +10,17 @@
     appId: "1:1066863130857:web:ca12a4a7cc7a2a04b11878",
     measurementId: "G-8EX0T9DQ0R"
   };
-  
+
   firebase.initializeApp(firebaseConfig);
 
     var database = firebase.database();
-    var nextMeetupContainer = document.getElementById('slackLink');
+    var slackButton = document.getElementById('slackLink');
 
     database.ref('slackInfo').on('value', function (snapshot)
     {
         var data = snapshot.val();
         var slackUrl = data.url;
 
-        console.log(slackUrl);
+        slackButton.setAttribute("href", slackUrl);
     });
 })();
